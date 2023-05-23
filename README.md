67fdee968ea397fe6093b60f50aef1d6aa0cf7ea
Melike Çevik <melike_cevik1@outlook.com>
2023-05-23 09:07:09 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# crypto_wallet_app
+
+A new Flutter project.
+
+## Design
+
+- Freepik - asylab
+- [App Design](https://www.freepik.com/free-psd/template-wallet-cryptocurrency-mobile-app_12385833.htm)
+
+## App
+
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'main_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MainApp(),
+    );
+  }
+}
+
